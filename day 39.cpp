d1e4861d6f98a370aa148ecfcc3d5321d4b75511
Abhi2839 <143724189+Abhi2839@users.noreply.github.com>
2024-10-12 15:54:16 +0530

    Add files via upload

---
+++ day 39.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include"bits/stdc++.h"
+int main()
+{
+    // std::cout<<"Hello World";
+double a; //kwh/day
+cin>>a;
+int b; //num of days
+cin>>b;
+float ans=pow(a,b);
+cout<<ans<<" kwh";
+    return 0;
+}
