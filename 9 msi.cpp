86ee87bc4f2ac684597edbacb0a5478d17f5c81d
Abhi2839 <143724189+Abhi2839@users.noreply.github.com>
2024-11-21 12:14:02 +0530

    Add files via upload

---
+++ 9 msi.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include "bits/stdc++.h"
+
+int main()
+{
+	int a;
+	cin>>a;
+	vector<int>v;
+
+	int k;
+	cin>>k;
+	for (int i=0; a>i; i++) {
+		int b;
+		cin>>b;
+		v.push_back(b);
+	}
+	sort(v.begin(),v.end());
+	cout<<v[v.size()-1-k];
+	return 0;
+}
