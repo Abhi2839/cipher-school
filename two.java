bcfa00b818b504e9d737683ebfae90e0062f89a8
Abhi2839 <143724189+Abhi2839@users.noreply.github.com>
2025-01-30 22:08:35 +0530

    Add files via upload

---
+++ two.java
@@ -0,0 +1,17 @@
+// Write a Java program to demonstrate the use of primitive data types and type conversion. The program should:
+
+// Declare and initialize variables of different primitive data types (int, double, char, and boolean).
+// Perform type conversion by converting an int to a double and a char to an int.
+// Print the values of all variables before and after the conversions.
+
+
+public class two {
+    public static void main(String[] args) {
+        int a=100;
+        System.out.println("Before "+a);
+        System.out.println("After "+(double)a);
+        char b='A';
+        System.out.println("Before: "+b);
+System.out.println("After " +(int)b);
+    }
+}
